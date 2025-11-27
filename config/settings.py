5aa7f2f96422bd758b6a201c2d5f3e2292e147bb
Shafiq1995 <shafiqshumon0@gmail.com>
2025-11-27 14:08:14 +0600

    Email verification 1

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = _env_bool("EMAIL_USE_TLS", True)
 EMAIL_USE_SSL = _env_bool("EMAIL_USE_SSL", False)
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL", DEFAULT_FROM_EMAIL or "futureskill100@gmail.com"
